df42815e38f223df886c97f76266c83e174f4e2d
RMJStudio <rmjarabo@yahoo.com>
2018-06-13 12:04:24 +0200

    Version13Junio2018

---
+++ Assets/Scripts/CSharpScript/MoverEsfera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverEsfera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public GameObject esfera;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+			this.transform.position=new Vector3(this.transform.position.x-0.1f,this.transform.position.y);
+}
+}
